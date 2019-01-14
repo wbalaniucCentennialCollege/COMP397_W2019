fac4fef2a2e14cde4ffdc080f0efa17ed3a9174e
wbalaniucCentennialCollege <wbalaniu@my.centennialcollege.ca>
2019-01-14 17:09:13 -0500

    Added EaselJS support

--- Scripts/core/game.js
+++ Scripts/core/game.js
@@ -5,9 +5,32 @@
     Anything in braces is in a closure. Won't go to global namespace.
 */
 (function () {
+    // Global Game Variables
+    var canvas = document.getElementById("canvas");
+    var stage;
+    var helloLabel;
     function Init() {
         console.log("Initialization start");
-        var x = 10;
+        Start();
+    }
+    function Start() {
+        console.log("Starting Application...");
+        // Initialize CreateJS
+        stage = new createjs.Stage(canvas);
+        createjs.Ticker.framerate = 60; // 60 FPS
+        createjs.Ticker.on("tick", Update);
+        Main();
+    }
+    function Update() {
+        helloLabel.rotation += 5;
+        stage.update();
+    }
+    function Main() {
+        console.log("Game Start...");
+        helloLabel = new createjs.Text("Hello World", "40px Consolate", "#000000");
+        helloLabel.x = 100;
+        helloLabel.y = 100;
+        stage.addChild(helloLabel);
     }
     window.onload = Init;
 })();
--- Scripts/core/game.ts
+++ Scripts/core/game.ts
@@ -6,11 +6,42 @@
 */
 (function() {
 
-    function Init() {
+    // Global Game Variables
+    let canvas = document.getElementById("canvas");
+    let stage:createjs.Stage;
+    let helloLabel: createjs.Text;
+
+    function Init():void {
         console.log("Initialization start");
 
-        let x: number = 10;
+        Start();
     }
+
+    function Start():void {
+        console.log("Starting Application...");
+
+        // Initialize CreateJS
+        stage = new createjs.Stage(canvas);
+        createjs.Ticker.framerate = 60; // 60 FPS
+        createjs.Ticker.on("tick", Update);
+        Main();
+    }
+
+    function Update():void {
+        helloLabel.rotation += 5;
+        stage.update();
+    }
+
+    function Main():void {
+        console.log("Game Start...");
+
+        helloLabel = new createjs.Text("Hello World", "40px Consolate", "#000000");
+        helloLabel.x = 100;
+        helloLabel.y = 100;
+
+        stage.addChild(helloLabel);
+    }
+
 
     window.onload = Init;
 })();
